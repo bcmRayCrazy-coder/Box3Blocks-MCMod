cfe424a92dbe60caf74926043e1721eb4f208b90
viyrs <2991883280@qq.com>
2026-02-06 22:18:18 +0800

    refactor(Box3Mod): 移除未使用的日志记录器并注释调试信息
    移除了未被使用的LOGGER静态变量，并将调试用的"Hello Fabric world!"日志信息注释掉，
    以减少不必要的控制台输出。
    
    feat(lang): 添加新的物品分类标签
    
    为en_us.json语言文件添加了9个新的物品分类标签，包括字母、数字、符号、颜色、元素、食物、
    光源、自然和结构等分类，用于更好地组织游戏中的物品分组。

--- src/main/java/com/box3lab/Box3Mod.java
+++ src/main/java/com/box3lab/Box3Mod.java
@@ -12,7 +12,7 @@ public class Box3Mod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	// public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
@@ -20,6 +20,6 @@ public class Box3Mod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 		ModBlocks.initialize();
-		LOGGER.info("Hello Fabric world!");
+		// LOGGER.info("Hello Fabric world!");
 	}
 }
