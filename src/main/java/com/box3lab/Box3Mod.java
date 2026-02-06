762a6e9ba3d73dc94e8987d81c5110e33267f20f
viyrs <2991883280@qq.com>
2026-02-06 16:47:14 +0800

    eat(Box3Mod): 初始化模块块注册
    新增ModBlocks类的导入并在初始化方法中调用initialize()方法，
    确保模块中的方块能够正确注册到游戏中。

--- src/main/java/com/box3lab/Box3Mod.java
+++ src/main/java/com/box3lab/Box3Mod.java
@@ -1,5 +1,6 @@
 package com.box3lab;
 
+import com.box3lab.register.ModBlocks;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -18,7 +19,7 @@ public class Box3Mod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		ModBlocks.initialize();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
