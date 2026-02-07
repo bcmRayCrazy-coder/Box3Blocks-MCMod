eb89c57a90896081bacf3aaec750cd0cbdaa176c
viyrs <2991883280@qq.com>
2026-02-07 15:45:18 +0800

    refactor(VoxelBlock): 移除未实现的entityInside方法
    移除了VoxelBlock类中未实现的entityInside方法，该方法仅包含
    TODO注释和UnsupportedOperationException异常抛出。
    
    同时清理了类中不再使用的Entity和Level导入语句。
    
    docs(fabric.mod.json): 更新模组元数据信息
    
    将模组名称从"Box3Mod"更新为"神奇代码岛方块集"
    更新描述为"导入神奇代码岛中的384个方块到我的世界"
    作者信息更新为"神岛实验室"
    添加了GitHub和问题跟踪链接

--- src/main/java/com/box3lab/block/VoxelBlock.java
+++ src/main/java/com/box3lab/block/VoxelBlock.java
@@ -2,10 +2,8 @@ package com.box3lab.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.Rotation;
@@ -48,8 +46,4 @@ public class VoxelBlock extends Block {
         return super.isCollisionShapeFullBlock(state, level, pos);
     }
 
-    public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'entityInside'");
-    }
 }
